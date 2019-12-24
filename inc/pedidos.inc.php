734bf9eefd47ef0fce54fbe294c14a197233e695
André Ferreira <56550873+falconnz@users.noreply.github.com>
2019-12-24 10:31:30 +0000

    master

--- inc/pedidos.inc.php
+++ inc/pedidos.inc.php
@@ -1,11 +1,11 @@
 
-   '<div class="container">
+   <div class="container">
         <div class="container-fluid py-3">
             <header class="col-md-12 mb-4">
                     <h2 class="text-center text-dark">Pedidos</h2>
                     <span class="underline mb-3"></span>
                                 </header>
-            <div class="row justify-content-center">';
+            <div class="row justify-content-center">
 
 
      
@@ -19,11 +19,11 @@ foreach ($dados as $row) {
                     <div class="card" style="width: 18rem">
                         <img src="img/exemplo.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h3 class="card-title grad-txt">Nome do Projeto: '.$row["nome_projeto"].'</h3><hr>
-                            <h4 class="card-title grad-txt">Descrição: '.$row["descricao"].'</h4><hr>
-                            <h4 class="card-title grad-txt">Utilizador: '.$row["nome"].'</h4><hr>
-                            <h4 class="card-title grad-txt">Orçamento: '.$row["preco"].'</h4>
-                            <a href="index.php?op=reqpage&id='.$row["id_utilizador"].'" class="btn btn-primary">Go somewhere</a>
+                            <h3 class="card-title grad-txt">Nome do Projeto:</h3><p class="text-dark card-subtitle">'.$row["nome_projeto"].'<p><hr>
+                            <h4 class="card-title grad-txt">Descrição:</h4><p class="text-dark card-subtitle">'.$row["descricao"].'</p><hr>
+                            <h4 class="card-title grad-txt">Utilizador:</h4><p class="text-dark card-subtitle">'.$row["nome"].'</p><hr>
+                            <h4 class="card-title grad-txt">Orçamento:</h4><p class="text-dark card-subtitle">'.$row["preco"].'€</p>
+                            <a href="index.php?op=reqpage&id='.$row["id_utilizador"].'" class="btn btn-primary mt-2">Go somewhere</a>
                         </div>
                     </div>
                 </div>';
--- inc/registar.inc.php
+++ inc/registar.inc.php
@@ -9,14 +9,14 @@
             <form class="col-6 text-center justify-content-center" method="POST" action="">
                 <input type="text" class="form-control" name="nome" placeholder="Nome" required>
                 <input type="text" class="form-control mt-2" name="apelido" placeholder="Apelido" required>
-                <select class="custom-select mt-2 text-secondary w-100 align-content-left" id="inputGroupSelect01" name="tipo_utilizador">
+                <select class="custom-select mt-2 text-secondary w-100 align-content-left" id="inputGroupSelect01" name="tipo_utilizador" required>
                     <option class="bg-dark" value="" disabled selected>Tipo de Registo</option>
                     <option class="text-secondary" value="1">Cliente</option>
                     <option class="text-secondary" value="2">Trabalhador</option>
                 </select>
                 <input type="text" class="form-control mt-2" name="user" placeholder="Username" required>
                 <input type="date" class="form-control mt-2" name="data_nascimento" placeholder="Data de Nascimento" required>
-                <input type="email" class="form-control mt-2" name="email" placeholder="mail" required>
+                <input type="email" class="form-control mt-2" name="email" placeholder="Email" required>
                 <input type="password" class="form-control mt-2" name="pass" placeholder="Password" required>
                 <button type="submit" name="submit" class="btn btn-grad grad text-center m-2"><i class="fas fa-sign-in-alt"></i> Registar</button>
             </form>

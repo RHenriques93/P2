978b9f9028cf7d56fb5dd8d24a99120bd5622933
André Ferreira <56550873+falconnz@users.noreply.github.com>
2019-12-24 17:16:33 +0000

    master

--- inc/user_page.inc.php
+++ inc/user_page.inc.php
@@ -1,49 +1,29 @@
+<div class="container">
+  <div class="container-fluid py-3 justify-content-center">
 <?php
-
-
 if(isset($_SESSION['nome'],$_SESSION['id_utilizador'])){
-
-  
-
-   
-
 require("db_projetofinal.php");
-
 
 $id = $_SESSION["id_utilizador"];
 
-
-
-
 echo '
-
-
-<div class="container">
-
-<div class="row">
-    <div class="col-md-12 mb-4">
-            <br>
-            <h2 class="text-center text-dark">Bem Vindo(a) '.$_SESSION["nome"].'</h2>
-            <span class="underline-rosa mb-3"></span>
-                                  
-    </div>
-
-   
-      <ul class="nav navbar-nav list-group">
-        <li class="list-group-item"><a href ="index.php?op=usersettings">Informações de Perfil</a></li>
-        <li class="list-group-item"><a href ="index.php?op=gerirservicos">Gerir Serviços</a></li>
-        <li class="list-group-item"><a href="index.php?op=logout">Logout</a></li>
-       </ul>
-               
-               
-    
-    </div>
-    </div>
+<header class="col-md-12 mb-4">
+  <h2 class="text-center text-dark">Bem Vindo(a) '.$_SESSION["nome"].'</h2>
+  <span class="underline-rosa mb-3"></span>
+</header>
+<div class="row justify-content-center">
+  <div class="col-md-4">
+    <button class="btn btn-grad grad col-12 mb-2"><h5><a class="text-light" href="index.php?op=usersettings">Informações de Perfil</a></h5></button>
+  </div>
+  <div class="col-md-4">
+    <button class="btn btn-grad grad col-12 mb-2"><h5><a class="text-light" href ="index.php?op=gerirservicos">Gerir Serviços</a></h5></button>
+  </div>
+  <div class="col-md-12 text-center my-2 mt-4">
+    <a class="grad-txt" href="index.php?op=logout"><h5>Logout</a></h5></button>
+  </div>
+</div>
 
 ';
-
-
-
 
 $dados = $db->query("SELECT * FROM utilizador WHERE utilizador.id_utilizador = '$id'");
 
@@ -52,29 +32,19 @@ foreach ($dados as $linha) {
 
  echo   '
 
-    <div class="container">
-    
-   
+    <div class="container">  
 </div>'; 
-
 }
 
+?>         
+  </div>
+</div>
 
 
-
-            ?>         
-            </div>
-        </div>
-    </div>
-
-
-  <?php
- } else {
-
+<?php
+  } else {
     header("location:index.php?op=login");
-
-}
-
+  }
 ?>
 
 

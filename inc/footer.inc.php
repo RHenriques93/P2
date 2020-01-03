d52818149073d0edefef4246919556fdfb98eac1
André Ferreira <56550873+falconnz@users.noreply.github.com>
2020-01-03 12:32:46 +0000

    master

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -26,8 +26,8 @@
     </div>
   </div>
   <div class="page-footer font-small bg-dark mt-4">
-<div class="footer-copyright text-center py-3 nova-s">© 2020 Copyright |
-    <a target="_blank" href="https://www.falcondesign.pt/" class="nova-s">André Ferreira</a> e <a href="https://pt.pornhub.com/gayporn" class="nova-s">Rafael Henriques</a>
+<div class="footer-copyright text-center py-3">© 2020 Copyright |
+    <a target="_blank" href="https://www.falcondesign.pt/">André Ferreira</a> e <a target="_blank" href="https://pt.pornhub.com/gayporn">Rafael Henriques</a>
   </div>
 </div>
 </div>

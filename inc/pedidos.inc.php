d73fa16293fcb2a65ffd7cf1e9c96bc3f543c9c4
André Ferreira <56550873+falconnz@users.noreply.github.com>
2020-01-03 15:46:43 +0000

    master

--- inc/pedidos.inc.php
+++ inc/pedidos.inc.php
@@ -13,7 +13,7 @@ foreach ($dados as $row) {
 
                 echo '<div class="col-md-4 col-sm-6 col-xs-12 mb-4">
                     <div class="card" >
-                        <img src="'.$row["imagem"].'" class="card-img-top" alt="...">
+                        <img src="'.$row["img_req"].'" class="card-img-top" alt="...">
                         <div class="card-body">
                            <p class="text-dark card-subtitle"><span class="font-weight-bold grad-txt">Nome do Projeto:</span> '.$row["nome_projeto"].'<p><hr>
                             <p class="text-dark card-subtitle"><span class="font-weight-bold grad-txt">Descrição: </span>'.$row["descricao"].'</p><hr>
